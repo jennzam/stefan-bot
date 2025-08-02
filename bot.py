a3ba3c046827b717e9b8c007ac51583df21ce265
jennzam <jenniferzamarron.s@gmail.com>
2025-08-02 03:50:22 -0500

    Update 2

--- bot.py
+++ bot.py
@@ -3,27 +3,37 @@ import os
 import random
 from dotenv import load_dotenv
 
+
+
 load_dotenv()
 
 class MyClient(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
 
+
     async def on_message(self, message):
         if message.author == self.user:
             return
 
         if message.content or message.attachments:
-            with open("stefan_quotes.txt", "r", encoding="utf-8") as file:
-                quotes = file.readlines()
+            try:
+                with open("stefan_quotes.txt", "r", encoding="utf-8") as file:
+                    quotes = file.readlines()
 
-            if quotes:
-                quote = random.choice(quotes).strip()
-                await message.channel.send(quote)
+                if quotes:
+                    quote = random.choice(quotes).strip()
+                    await message.channel.send(quote)
+            except Exception as e:
+                print(f"Error sending message: {e}")
 
 
 intents = discord.Intents.default()
 intents.message_content = True
 
+
 client = MyClient(intents=intents)
 client.run(os.getenv("DISCORD_TOKEN"))
+
+
+
